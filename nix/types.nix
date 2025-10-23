94afbbd0f3b2a6550d6db9c9ef94661f527f35a0
Victor Borja <vborja@apache.org>
2025-10-23 13:58:58 -0600

    expose aspectSubmodule

--- nix/types.nix
+++ nix/types.nix
@@ -2,64 +2,78 @@ lib:
 let
 
   aspectsType = lib.types.submodule {
-    freeformType = lib.types.lazyAttrsOf aspectSubmoduleType;
+    freeformType = lib.types.lazyAttrsOf aspectSubmodule;
   };
 
-  providerType = lib.types.functionTo aspectSubmoduleType;
+  providerType = lib.types.functionTo aspectSubmodule;
 
-  aspectSubmoduleType = lib.types.submodule (
-    {
-      name,
-      aspect,
-      config,
-      ...
-    }:
-    {
-      freeformType = lib.types.lazyAttrsOf lib.types.deferredModule;
-      config._module.args.aspect = config;
-      options.name = lib.mkOption {
-        description = "Aspect name";
-        default = name;
-        type = lib.types.str;
-      };
-      options.description = lib.mkOption {
-        description = "Aspect description";
-        default = "Aspect ${name}";
-        type = lib.types.str;
-      };
-      options.includes = lib.mkOption {
-        description = "Providers to ask aspects from";
-        type = lib.types.listOf providerType;
-        default = [ ];
-      };
-      options.provides = lib.mkOption {
-        description = "Providers of aspect for other aspects";
-        default = { };
-        type = lib.types.submodule (
-          { config, ... }:
+  aspectSubmodule = aspectSubmoduleWithModules [ ];
+
+  aspectSubmoduleWithModules =
+    otherModules:
+    lib.types.submoduleWith {
+      shorthandOnlyDefinesConfig = true;
+      modules = otherModules ++ [
+        (
           {
-            freeformType = lib.types.lazyAttrsOf (lib.types.functionTo lib.types.unspecified);
-            config._module.args.provides = config;
-            options.itself = lib.mkOption {
-              readOnly = true;
-              description = "Provides itself";
-              type = providerType;
-              default = _: aspect;
+            name,
+            aspect,
+            config,
+            ...
+          }:
+          {
+            freeformType = lib.types.lazyAttrsOf lib.types.deferredModule;
+            config._module.args.aspect = config;
+            options.name = lib.mkOption {
+              description = "Aspect name";
+              default = name;
+              type = lib.types.str;
+            };
+            options.description = lib.mkOption {
+              description = "Aspect description";
+              default = "Aspect ${name}";
+              type = lib.types.str;
+            };
+            options.includes = lib.mkOption {
+              description = "Providers to ask aspects from";
+              type = lib.types.listOf providerType;
+              default = [ ];
+            };
+            options.provides = lib.mkOption {
+              description = "Providers of aspect for other aspects";
+              default = { };
+              type = lib.types.submodule (
+                { config, ... }:
+                {
+                  freeformType = lib.types.lazyAttrsOf (lib.types.functionTo lib.types.unspecified);
+                  config._module.args.provides = config;
+                  options.itself = lib.mkOption {
+                    readOnly = true;
+                    description = "Provides itself";
+                    type = providerType;
+                    default = _: aspect;
+                  };
+                }
+              );
+            };
+            options.__functor = lib.mkOption {
+              internal = true;
+              visible = false;
+              description = "Functor to default provider";
+              type = lib.types.functionTo lib.types.unspecified;
+              default = _: aspect.provides.itself;
             };
           }
-        );
-      };
-      options.__functor = lib.mkOption {
-        internal = true;
-        visible = false;
-        description = "Functor to default provider";
-        type = lib.types.functionTo lib.types.unspecified;
-        default = _: aspect.provides.itself;
-      };
-    }
-  );
+        )
+      ];
+    };
 
 in
 {
-  inherit aspectsType aspectSubmoduleType providerType;
+  inherit
+    aspectsType
+    aspectSubmodule
+    aspectSubmoduleWithModules
+    providerType
+    ;
 }
