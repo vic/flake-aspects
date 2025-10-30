520807b105e594df555c8e0d5b7019baebdbb1c4
Victor Borja <vborja@apache.org>
2025-10-30 11:49:06 -0600

    test including other aspect functor

--- checkmate.nix
+++ checkmate.nix
@@ -394,6 +394,48 @@
           {
             inherit expr expected;
           };
+
+        aspects."test override default provider includes" =
+          let
+            flake = mkFlake {
+              flake.aspects =
+                { aspects, ... }:
+                {
+                  aspectOne =
+                    { aspect, ... }:
+                    {
+                      classOne.bar = [ "should-not-be-present" ];
+                      includes = [ aspects.aspectTwo ];
+                      __functor =
+                        _:
+                        # deadnix: skip
+                        { class, aspect-chain }:
+                        {
+                          includes = [
+                            { classOne.bar = [ "from-functor" ]; }
+                          ] ++ map (f: f { message = "hello"; }) aspect.includes;
+                        };
+                    };
+                  aspectTwo.__functor =
+                    _:
+                    { message }:
+                    # deadnix: skip
+                    { class, aspect-chain }:
+                    {
+                      classOne.bar = [ message ];
+                    };
+                };
+            };
+
+            expr = (evalMod "classOne" flake.modules.classOne.aspectOne).bar;
+            expected = [
+              "hello"
+              "from-functor"
+            ];
+          in
+          {
+            inherit expr expected;
+          };
       };
 
     };
