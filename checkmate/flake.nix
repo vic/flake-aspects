56e0a85c571a4050e3f8ef3ba6d11c19805b0e4a
Victor Borja <vborja@apache.org>
2025-11-20 21:34:29 -0600

    update checkmate

--- checkmate/flake.nix
+++
@@ -1,7 +0,0 @@
-{
-  inputs.target.url = "path:..";
-  inputs.checkmate.url = "github:vic/checkmate";
-  inputs.checkmate.inputs.target.follows = "target";
-  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
-  outputs = inputs: inputs.checkmate.lib.newFlake;
-}
