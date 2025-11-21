9efdc39329e43289925636c50d5a909527aad347
Victor Borja <vborja@apache.org>
2025-11-20 18:33:06 -0600

    expose resolved modules on each aspect (#16)

--- checkmate.nix
+++ checkmate.nix
@@ -228,6 +228,50 @@
             inherit expr expected;
           };
 
+        aspects."test modules resolved" =
+          let
+            flake = mkFlake {
+              flake.aspects =
+                { aspects, ... }:
+                {
+                  aspectOne = {
+                    name = "one";
+                    includes = [ aspects.aspectOne.provides.dos ];
+                    classOne.bar = [ "zzz" ];
+                    provides.dos =
+                      { aspect-chain, ... }:
+                      {
+                        name = "dos";
+                        includes = [ aspects.aspectOne.provides.tres ];
+                        classOne.bar = map (x: x.name) aspect-chain;
+                      };
+
+                    provides.tres =
+                      { aspect-chain, ... }:
+                      {
+                        name = "tres";
+                        classOne.bar = [ (lib.last aspect-chain).name ];
+                      };
+                  };
+                };
+            };
+            mod = {
+              imports = [
+                fooOpt
+                (flake.aspects.aspectOne.modules.classOne)
+              ];
+            };
+            expr = lib.sort (a: b: a < b) (evalMod "classOne" mod).bar;
+            expected = [
+              "dos"
+              "one"
+              "zzz"
+            ];
+          in
+          {
+            inherit expr expected;
+          };
+
         aspects."test provides" =
           let
             flake = mkFlake {
