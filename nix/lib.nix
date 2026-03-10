81a51a8997abe392b9d0794424a4823adc9bd3af
Victor Borja <vborja@apache.org>
2026-03-10 17:27:36 -0600

    perf(core): Some optimizations (#40)
    Now 1.16x times faster than previous main. 
    
    ``` 
     hyperfine -m 20 -w 10  -n perf "cd perf && just ci" -n main "cd main && just ci"
    Benchmark 1: perf
      Time (mean ± σ):     114.5 ms ±   1.7 ms    [User: 77.2 ms, System: 32.3 ms]
      Range (min … max):   111.7 ms … 119.3 ms    25 runs
    
    Benchmark 2: main
      Time (mean ± σ):     133.3 ms ±   1.7 ms    [User: 91.7 ms, System: 36.5 ms]
      Range (min … max):   130.9 ms … 137.2 ms    22 runs
    
    Summary
      perf ran
        1.16 ± 0.02 times faster than main
    
    ```

--- nix/lib.nix
+++ nix/lib.nix
@@ -1,27 +1,15 @@
-# Public API entry point for flake-aspects library
-# Exports: types, transpose, aspects, new, new-scope
 lib:
 let
-  # Type system: aspectsType, aspectSubmodule, providerType
   types = import ./types.nix lib;
-
-  # Generic transposition utility: parameterized by emit function
+  resolve = import ./resolve.nix lib;
   transpose =
     {
       emit ? lib.singleton,
     }:
     import ./default.nix { inherit lib emit; };
-
-  # Aspect transposition with resolution
   aspects = import ./aspects.nix lib;
-
-  # Dynamic class forwarding into submodules
   forward = import ./forward.nix lib;
-
-  # Low-level scope factory: parameterized by callback
   new = import ./new.nix lib;
-
-  # High-level named scope factory
   new-scope = import ./new-scope.nix new;
 in
 {
@@ -32,5 +20,6 @@ in
     new
     new-scope
     forward
+    resolve
     ;
 }
